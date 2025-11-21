7bb0e65d0e9f5f789d63d2ff49f2c84118a3a131
VAN KHAI NGUYEN <88942454+Khaiisme@users.noreply.github.com>
2025-11-21 14:39:45 +0000

    .

--- pos365/src/App.jsx
+++ pos365/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Table from "./components/Table";
 import Modal from "./components/Modal";
 import { FiRotateCcw } from 'react-icons/fi';
